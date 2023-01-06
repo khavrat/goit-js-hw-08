b5aaa7c0155d6de62df6551864ce2adcf6f2e5fe
khavrat <ihavrat@gmail.com>
2023-01-06 17:55:32 +0200

    added 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+let currentStorageValue = 0;
+
+player.on('timeupdate', throttle(playerOn, 1000));
+
+function playerOn(e) {
+  const currentTime = e.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTime);
+
+  const durationTime = e.duration;
+  if (currentTime === durationTime) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function getSavedCurrentTime() {
+  currentStorageValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  return currentStorageValue;
+}
+
+getSavedCurrentTime();
+
+player.setCurrentTime(currentStorageValue);
