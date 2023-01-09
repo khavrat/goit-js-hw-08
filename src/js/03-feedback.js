4638d9db3a305a1110c1c8d64f5656036529dd50
khavrat <ihavrat@gmail.com>
2023-01-09 22:06:11 +0200

    added 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputValue, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+getSavedFormData();
+
+function onInputValue(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function getSavedFormData(e) {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+  if (savedData !== null) {
+    input.value = parsedData.email;
+    textarea.value = parsedData.message;
+  }
+}
+
+function onFormSubmit(e) {
+  formData.email = input.value;
+  formData.message = textarea.value;
+  console.log(formData);
+
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
